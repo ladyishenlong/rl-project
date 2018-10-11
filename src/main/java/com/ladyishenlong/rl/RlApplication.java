0005ece48a68a6e0bc706cc5c707b6abc3947e01
ruanchenhao <ruanchenhao@sanjiang.com>
2018-10-11 09:25:12 +0800

    初次提交

---
+++ src/main/java/com/ladyishenlong/rl/RlApplication.java
@@ -0,0 +1,14 @@
+package com.ladyishenlong.rl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class RlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RlApplication.class, args);
+    }
+}
