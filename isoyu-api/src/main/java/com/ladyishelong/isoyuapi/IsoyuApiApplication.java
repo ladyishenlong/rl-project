f70dd969582e799567a9fd863edd5393a08f4148
ruanchenhao <ruanchenhao@sanjiang.com>
2018-10-11 11:48:37 +0800

    springcloud

---
+++ isoyu-api/src/main/java/com/ladyishelong/isoyuapi/IsoyuApiApplication.java
@@ -0,0 +1,14 @@
+package com.ladyishelong.isoyuapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class IsoyuApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsoyuApiApplication.class, args);
+    }
+}
